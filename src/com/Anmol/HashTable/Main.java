55e3fb81b9000c98d9d99398616f6b7b9f8456c4
Anmol Sharma <53795146+Anmol-tech@users.noreply.github.com>
2020-04-08 18:49:40 +0530

    ! FirstNonRepeatChar

---
+++ src/com/Anmol/HashTable/Main.java
@@ -0,0 +1,11 @@
+package com.Anmol.HashTable;
+
+public class Main {
+
+    public static void main(String[] args) {
+        FirstNonRepeatChar obj = new FirstNonRepeatChar();
+        String str = "asaaetefs";
+        System.out.println(obj.findNonRepeatChar(str));
+
+    }
+}
